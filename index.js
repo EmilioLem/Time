8711e1615499d45ba92fb622aee39db30ad7fb16
emilemus <92602556+EmilioLem@users.noreply.github.com>
2022-03-22 19:00:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const drums = document.getElementById("drums");
 const main = document.getElementById("main");
-var perSer = null; //array con preguntas del personaje seleccionado
+var perSer = null; //array con preguntas del personaje seleccionado Sirve?
 var enConf = false; //Si config está abierto
 
 const formatQuestion = `<h1 id="Dquestion"></h1> <br> <div id="Countdown"></div> <br> <div id="dSVD"> </div> <br> <button id="Dopcion1" onclick="pregStart()"></button> <button id="Dopcion2" onclick="pregStart()"></button> <button id="Dopcion3" onclick="pregStart()"></button>`;
@@ -19,6 +19,8 @@ function asigPer(perSelected){ //Activado con opciones de personajes
     if(perSer==null){ //raro, porque asumimos que hay datos de todo
       //Sólo agrega los personajes al optCp si es la primera vez, o sea que no se ha seleccionado ningún personaje antes
       document.getElementById("optCp").innerHTML = `<details> <summary><h3>Cambio de personaje:</h3></summary> <div class="tablaP" onclick="asigPer('menor')">Menor de edad <br>${menor[0]} </div> <div class="tablaP" onclick="asigPer('universitario')">Universitario <br>${universitario[0]} </div> <div class="tablaP" onclick="asigPer('gobernante')">Gobernante <br>${gobernante[0]} </div> <div class="tablaP" onclick="asigPer('trabajador')">Empleado <br>${trabajador[0]} </div> <div class="tablaP" onclick="asigPer('empresario')">Empresario <br>${empresario[0]} </div> <div class="tablaP" onclick="asigPer('educador')">Educador <br>${educador[0]} </div></details>`;
+      //Esta parte agrega la lista de personajes a la parte del engranaje
+      //Sólo evita que haya dos lugares donde cambiar de personaje
     }
     switch(perSelected){
       case 'menor': perSer = menor; break;
@@ -46,7 +48,7 @@ function asigPer(perSelected){ //Activado con opciones de personajes
   
   if(enConf){
     document.getElementById("optC").style.display = "none";
-    drums.play();
+    //drums.play(); //What! the music is ended anyways.
     enConf = false;
   }
 
@@ -89,13 +91,13 @@ document.getElementById("optB").addEventListener("click", ()=>{
   let s10_countDown = document.getElementById("10_s_countDown"); //Repetida de pregStart()
   if(!enConf){ //abrir
     //Agregar estilo para resaltar engranaje
-    s10_countDown.pause();
+    if(perSer!=null) s10_countDown.pause();
     document.getElementById("optC").style.display = "initial";
     drums.play();
     enConf = true;
   }else{//cerrar
     //Quitar estilo de engranaje que lo resaltaba
-    s10_countDown.play();
+    if(perSer!=null) s10_countDown.play();
     document.getElementById("optC").style.display = "none";
     if(perSer) drums.pause();
     enConf = false;
